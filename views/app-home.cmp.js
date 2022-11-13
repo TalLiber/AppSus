05d871c3044a2cd0415d5bccf079542b1595183d
Tal Liber <tal.liber@gmail.com>
2022-11-13 07:01:30 +0200

    c

--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -1,6 +1,6 @@
 export default {
     template: `
-        <section class="home-page flex space-even">
+        <!-- <section class="home-page flex space-even">
         <router-link to="/mail/list?tab=inbox">
             <div class="gmail-wrapper">
                 <img  :src="gmailUrl" alt="" />
@@ -13,6 +13,29 @@ export default {
             </div>
         </router-link>
 
+        </section> -->
+        <section class="page-container">
+            <section class="content-container">
+                <h1>Secure, smart, and easy to use our apps</h1>
+                <p>Get more done with Gmail or Save your thoughts, wherever you are with google Keep</p>
+
+                <section class="btn-container">
+                    <span class="router-btn">
+                        <router-link to="/mail/list?tab=inbox">
+                            {{ 'Start Gmail' }}
+                        </router-link>
+                    </span>
+                    
+                    <span class="router-btn">
+                        <router-link to="/keep/notes">
+                            {{ 'Start Google Keep' }}
+                        </router-link>
+                    </span>
+                </section>
+            </section>
+            <section class="img-container">
+                <img src="assets/img/googleHome.webp" />
+            </section>
         </section>
     `,
     data() {
